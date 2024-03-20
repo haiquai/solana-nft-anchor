532c7a2fb894d4b289ef87d07ab367374c279972
haiquai <164226278+haiquai@users.noreply.github.com>
2024-03-21 00:30:05 +0700

    Update lib.rs

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("71GFvBy6e9iVLSLsUfJ9cTuX6ycufHexmQsniMP46pVB");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
